56c60accd6d1042ff3d91044c1e054e0c88f28dd
mdadilakhtar <mdadila188@gmail.com>
2024-11-08 16:59:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to registration button
+document.querySelector('#registration button').addEventListener('click', () => {
+    // Open registration form in a new window
+    window.open('registration.html', '_blank');
+});
